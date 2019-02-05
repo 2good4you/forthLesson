51329bf6f5f3e7e260969e8049d63b6dc061c1dd
Bogdan Petrenko <pebogdan@ukr.net>
2019-02-05 16:07:13 +0200

    another option'edition
    '

---
+++ 4.1.1.php
@@ -0,0 +1,41 @@
+<?php
+
+function makingRhombus($value) : void
+{
+    $counter = 0;
+
+    while ($counter < $value ) {
+        echo str_repeat(' ', $value - $counter);
+        echo str_repeat(' *', ++$counter);
+        echo PHP_EOL;
+        $counter++;
+    }
+
+    $counter = 1;
+    while ($value > $counter ) {
+        $counter++;
+        echo str_repeat(' ', ++$counter);
+        $times = $value - $counter;
+        echo str_repeat(' *', ++$times);
+        echo PHP_EOL;
+    }
+}
+
+$times = (int)$argv[1];
+
+switch ($times) {
+    case $times === false:
+        echo 'U need to enter ODD value, after "3"' . PHP_EOL;
+        break;
+    case $times < 0:
+        echo 'U need to enter a positiv numer' . PHP_EOL;
+        break;
+    case $times === 1:
+        echo ' * ' . PHP_EOL;
+        break;
+    case $times % 2 === 0:
+        echo 'Only ODD numbers' . PHP_EOL;
+        break;
+    default:
+        makingRhombus($times);
+}
